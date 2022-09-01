2b29eab9e724ac064cd4752c9d55c2ad7502a790
mal-moo <malinmoohwagwa@gmail.com>
2022-09-01 17:21:57 +0900

    Update annotation

--- cafe/views.py
+++ cafe/views.py
@@ -1,13 +1,14 @@
-# from django.db import transaction
-# from rest_framework import generics, status
-# from rest_framework.permissions import AllowAny
-# from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import CafeSerializer
 from .models import Cafe
 
-
+"""
+viewsets을 이용하여 CRUD API 만들기
+"""
 class CafeViewset(viewsets.ModelViewSet):
+    """
+    리스펀스 메시지 핸들링 공부 필요
+    """
     queryset = Cafe.objects.all()
     serializer_class = CafeSerializer
-    http_method_names = ['get', 'post', 'put', 'patch']
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
