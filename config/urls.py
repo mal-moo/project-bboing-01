6d1e8d414d8825a3558c0ec959e88ae9532b5dfb
mal-moo <malinmoohwagwa@gmail.com>
2022-10-13 20:54:01 +0900

    Update swagger docs

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('cafe/', include('cafe.urls')),
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
 ]
