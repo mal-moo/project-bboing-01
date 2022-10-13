a090454c1e6b1e4302afb04f9421fa6760963d3b
mal-moo <malinmoohwagwa@gmail.com>
2022-10-13 21:08:32 +0900

    Update serializer.datetimefield option, Refact file path of custom model

--- common/models.py
+++ common/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class UnsignedBigAutoField(models.BigAutoField):
-    
+
+class UnsignedBigAutoField(models.BigAutoField):    
     def db_type(self, connection):
         return 'bigint(20) UNSIGNED AUTO_INCREMENT'
         
