5162a0d94ee081374f37181344286c82d4f70e28
mal-moo <malinmoohwagwa@gmail.com>
2022-10-24 22:15:19 +0900

    test feature1

--- config/urls.py
+++ config/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+# test test
